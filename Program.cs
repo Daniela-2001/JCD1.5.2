e25941e6d2f96cc4b840126fc40d97c40108d68d
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-10 10:30:29 +0000

    JCD1.5.2

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace JCD1._5._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Criar uma matriz para armazenar os números 
+            int[] numeros = new int[5];
+
+            //Receber cinco números do usuário
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Digite o número {0}: ", i + 1);
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Ordenar a matriz
+            Array.Sort(numeros);
+
+            //Exibir os números ordenados
+            Console.WriteLine("\nNúmeros ordenados:");
+            foreach (var numero in numeros)
+            {
+                Console.Write(numero + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
